d7b4ae91c2e3272784c313bb5d783f98935594b6
boosun13 <double0two22@gmail.com>
2023-09-03 16:56:16 +0900

    chore: DB として postgres を実装

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
+from database import init_db
 
 app = Flask(__name__)
+app.config.from_object('config.Config')
 
+# DB init
+init_db(app)
 
 @app.route("/")
 def hello_world():
---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+
+class DevConfig:
+    # SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{password}@{host}/{dbname}?charset=utf8'.format(**{
+        'user': os.getenv('DB_USER', 'root'),
+        'password': os.getenv('DB_PASSWORD', ''),
+        'host': os.getenv('DB_HOST', 'localhost'),
+        'dbname': os.getenv('DB_NAME')
+    })
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
+
+Config = DevConfig
