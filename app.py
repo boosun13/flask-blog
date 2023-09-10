b9d879b775aecc714474ca0a2a5114e55539fee5
boosun13 <double0two22@gmail.com>
2023-09-10 22:03:02 +0900

    fix: db が接続されなかったのを解消

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
+from pathlib import Path
 from flask import Flask
-from database import init_db
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
-app.config.from_object('config.Config')
+app.config.from_mapping(
+    SQLALCHEMY_DATABASE_URI=f"postgresql://{os.getenv('DB_USER')}:{os.getenv(
+        'DB_PASSWORD')}@db:5432/{os.getenv('DB_NAME')}",
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+)
 
-# DB init
-init_db(app)
+# DB migration
+db.init_app(app)
+Migrate(app, db)
+
 
 @app.route("/")
 def hello_world():
