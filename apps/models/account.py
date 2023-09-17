f7e06bbdb3ed06f3000bf397e8265df70d4dcff8
boosun13 <double0two22@gmail.com>
2023-09-17 17:02:22 +0900

    chore: ulid に使用するライブラリを変更

--- apps/models/account.py
+++ apps/models/account.py
@@ -1,8 +1,9 @@
 from apps.app import db
 from datetime import datetime
+from ulid import ULID
 
 class Account(db.Model):
     __tablename__ = 'accounts'
 
-    id = db.Column(db.String, primary_key=True)
+    id = db.Column(db.String(26), primary_key=True, default=str(ULID()))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
