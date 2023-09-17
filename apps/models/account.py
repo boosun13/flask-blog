13a152537b55e6295ab3c678e08535f849279635
boosun13 <double0two22@gmail.com>
2023-09-17 17:33:44 +0900

    fix: accounts テーブル作成のバージョニングを修正

--- apps/models/account.py
+++ apps/models/account.py
@@ -2,8 +2,10 @@ from apps.app import db
 from datetime import datetime
 from ulid import ULID
 
+
 class Account(db.Model):
     __tablename__ = 'accounts'
+    __table_args__ = {'comment': 'アカウント情報'}
 
     id = db.Column(db.String(26), primary_key=True, default=str(ULID()))
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
