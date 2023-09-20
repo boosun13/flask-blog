08c3628e7f06c8b7841e8d73064ae5a2e42ee9be
boosun13 <double0two22@gmail.com>
2023-09-20 22:15:28 +0900

    chore: accounts テーブルの column を追加。マイグレーションファイルもリセット

--- apps/models/account.py
+++ apps/models/account.py
@@ -1,11 +1,12 @@
 from apps.app import db
 from datetime import datetime
 from ulid import ULID
-
+from pytz import timezone
 
 class Account(db.Model):
     __tablename__ = 'accounts'
     __table_args__ = {'comment': 'アカウント情報'}
 
     id = db.Column(db.String(26), primary_key=True, default=str(ULID()))
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+    joined_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone('Asia/Tokyo')), comment='登録日時')
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone('Asia/Tokyo')))
