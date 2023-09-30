fa02a79b39a05eed563c68f553afc0d9ca4f172c
boosun13 <double0two22@gmail.com>
2023-09-30 16:41:56 +0900

    feat: 確認用のエンドポイントを変更

--- apps/app.py
+++ apps/app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from pytz import timezone
 import os
 
 db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config['TIMEZONE'] = 'Asia/Tokyo'
+
 app.config.from_mapping(
     SQLALCHEMY_DATABASE_URI=f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@db:5432/{os.getenv('DB_NAME')}",
     SQLALCHEMY_TRACK_MODIFICATIONS=False,
@@ -21,19 +24,14 @@ from apps.models.account_deletion import AccountDeletion
 from apps.models.user import User
 
 @app.route("/")
-def hello_world():
-    a = Account.query.count()
+def current_account():
+    account = Account.query.first()
 
-    if a == 0:
-        # Create a new account
-        account = Account()
-        db.session.add(account)
-        db.session.commit()
-        a = Account.query.count()
-
-    return f'<p>{a}</p>'
+    return f'<p>{account.id}</p>'
 
 
-@app.route('/user/<username>')
-def profile(username):
-    return f'{username}\'s profile'
+@app.route('/account/<id>')
+def account(id):
+    account = Account.query.get(id)
+
+    return f'This user\'s current email is \'{account.current_user.email}\''
