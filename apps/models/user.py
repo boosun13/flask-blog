bc4b56ee61dba8aca1bc70f572ff48d4504c6b8b
boosun13 <double0two22@gmail.com>
2023-10-01 22:24:28 +0900

    feat: users テーブルにパスワードcolumnを追加

--- apps/models/user.py
+++ apps/models/user.py
@@ -2,6 +2,8 @@ from apps.app import db
 from datetime import datetime
 from ulid import ULID
 from pytz import timezone
+from werkzeug.security import generate_password_hash
+
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -10,9 +12,19 @@ class User(db.Model):
     id = db.Column(db.String(26), primary_key=True, default=str(ULID()))
     account_id = db.Column(db.String(26),db.ForeignKey('accounts.id'), nullable=False, index=True)
     email = db.Column(db.String(255), nullable=False, comment='登録メールアドレス')
+    password_hash = db.Column(db.String, comment='ハッシュ化されたパスワード')
     registered_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone('Asia/Tokyo')), comment='登録日時')
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone('Asia/Tokyo')))
 
     # relationships
     account = db.relationship('Account', back_populates='users', lazy=True, uselist=False)
+
+    # NOTE: property for password
+    @property
+    def password(self):
+        raise AttributeError('password: write-only field')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
 
